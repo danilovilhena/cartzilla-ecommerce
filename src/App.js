0b07c65f2b52a703cebdacba7e8764e87ac04476
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2022-03-01 20:20:22 -0300

    Including Husky, Prettier and Lint-Staged

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
-}
+  return <div className="App">App</div>;
+};
 
 export default App;
